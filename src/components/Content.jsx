0133bcceb08032d3842e1f3d526581f608d3d93b
Matheus Repizo <matheusdprepizo@gmail.com>
2022-10-07 15:54:27 -0300

    Pontapé Inicial

---
+++ src/components/Content.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+class Content extends React.Component {
+  state = { };
+
+  render() {
+    return (
+      <Switch>
+        <Route path="/" component="" />
+
+      </Switch>
+
+    );
+  }
+}
+
+export default Content;
