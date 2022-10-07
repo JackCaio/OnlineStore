c58ea578263a6acfd31fabc8b6c6e8b7dd997970
CalebeLAR <ca6ele@gmail.com>
2022-10-07 17:41:36 -0400

    requisito 04 ainda sem estilo

---
+++ src/components/ ListCategories.jsx
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import { getCategories } from '../services/api';
+
+export default class ListCategories extends Component {
+  constructor() {
+    super();
+    this.categories = this.categories.bind(this);
+    this.state = {
+      categories: [],
+    };
+  }
+
+  componentDidMount() {
+    console.log('lalalal');
+    this.categories();
+  }
+
+  async categories() {
+    const request = await getCategories();
+    this.setState({
+      categories: request,
+    });
+  }
+
+  render() {
+    const { categories } = this.state;
+    return (
+      <div
+        style={
+          { height: '100vh',
+            width: '300px',
+            marginRight: '50px',
+            display: 'inline-block',
+            backgroundColor: 'blue' }
+        }
+      >
+        <h1>ListCategories</h1>
+        {
+          categories.map((category) => (
+            <li key={ category.id }>
+              <label htmlFor={ `radio${category.id}` } data-testid="category">
+                <input type="radio" name="radio" id={ `radio${category.id}` } />
+              </label>
+              {category.name}
+            </li>
+          ))
+        }
+      </div>
+    );
+  }
+}
