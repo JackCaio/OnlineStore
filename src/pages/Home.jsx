7d4e0378ba5d3abec958d3f070ac9147999ef153
JackCaio <caio.iack1@gmail.com>
2022-10-09 19:42:09 -0300

    style: Adiciona Header para pagina

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
+import Header from '../components/Header';
 import ListCategories from '../components/ListCategories';
 import ProductList from '../components/ProductList';
 import { getProductsFromCategoryAndQuery } from '../services/api';
@@ -39,6 +40,7 @@ export default class Home extends Component {
     const { productsList, prodSearch, categoryId } = this.state;
     return (
       <div className="home__display">
+        <Header />
         <ListCategories
           categoryId={ categoryId }
           handleInputChange={ this.handleInputChange }
