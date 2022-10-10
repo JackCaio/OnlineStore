e354e744244ea8f3a10f8c384dddd999a0caab97
JackCaio <caio.iack1@gmail.com>
2022-10-10 16:17:09 -0300

    feat: Funcao de busca por produto

--- src/services/api.js
+++ src/services/api.js
@@ -13,7 +13,11 @@ export async function getProductsFromCategoryAndQuery(categoryId, query) {
   return response;
 }
 
-export async function getProductById() {
+export async function getProductById(id) {
   // Esta implementação específica não é avaliada, mas pode ajudar você 🙂
   // Atenção: essa função não deverá ser chamada na tela do carrinho de compras.
+  const url = `https://api.mercadolibre.com/items/${id}`;
+  const request = await fetch(url);
+  const response = await request.json();
+  return response;
 }
