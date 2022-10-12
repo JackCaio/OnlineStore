538ceaa90275d943d4d4fc3cdd97051f5af95825
JackCaio <caio.iack1@gmail.com>
2022-10-12 15:54:17 -0300

    feat: Adiciona tela de checkout

---
+++ src/pages/Checkout.jsx
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+
+export default class Checkout extends Component {
+  constructor() {
+    super();
+    this.state = {
+      cartList: (localStorage.getItem('products'))
+        ? JSON.parse(localStorage.getItem('products')) : [],
+    };
+  }
+
+  render() {
+    const { cartList } = this.state;
+    const total = cartList
+      .reduce((acc, cur) => acc + (cur.quantityPurchased * cur.price), 0);
+    return (
+      <div>
+        <div>
+          {cartList.map((prod) => (
+            <p key={ prod.id }>
+              {`${prod.title} R$${prod.price * prod.quantityPurchased}`}
+            </p>
+          ))}
+          <p>{`Total: R$${total}`}</p>
+        </div>
+      </div>
+    );
+  }
+}
